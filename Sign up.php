a61675fa44f225f37ce6d13c2d47085e7b949d81
Bida21-004 <100191176+Bida21-004@users.noreply.github.com>
2023-05-23 18:56:08 +0200

    Add files via upload

---
+++ Sign up.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>UNI-LOBBY</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../R & I/uniLobby.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+</head>
+<body>
+
+<div class="topnav">
+    <div class="topnav" id="myNavbar">
+      <div class="logo">
+        <a href="#home" class="logo" >UNI LOBBY</a>
+      </div>
+        <!-- Right-sided navbar links -->
+        <div class="links">
+          <a href="http://localhost/login.php" class="link1">LOG IN</a>
+          <a href="#row" class="link2">ABOUT</a>
+          <a href="#contact" class="link2">CONTACT</a>
+        </div>
+    </div>
+</div>
+
+<div class="header" id="home">
+  <div class="header2">
+    <h1>WELCOME TO<br>UNI LOBBY</h1>
+    <p>We Are Here To Help.</p>
+    <a href="http://localhost/register.php"<button class="button">Sign Up</button></a>
+  </div>
+</div>
+
+<div class="row">
+  <div class="about" id="row">
+    <h2>WHAT IS UNI LOBBY</h2>
+    <p>UniLobby is a web application designed to help prospective students select courses based<br>
+      on their grades and gives them insights on which universities that offer those courses.
+      <br><br>The web application is designed to help students in course selection.<br><br>The system aims to
+      provide the best possible results for course selection for inexperienced prospects to make.<br><br>
+      UniLobby aims to incorporate these factors and provide personalized course
+      recommendations to help students make informed decisions.<br><br>By leveraging these research findings, UniLobby can provide a valuable resource to students in their academic
+      journey
+</p>
+   </div>
+  
+  <div class="Footer" id="contact">
+    <div class="row1">
+      <div class="info">
+        <i class="fa fa-map-marker" style="width:30px"></i>Unilobby<br>
+        <i class="fa fa-phone" style="width:30px"></i> Phone: +267 151515<br>
+        <i class="fa fa-envelope" style="width:30px"> </i> Email: unilobby@gmail.com<br>
+      </div>
+    </div>
+    <div class="row2">
+      <p>Give Us A Follow</p>
+      <div class="social">
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-codepen"></i></a>
+      </div>
+    </div>
+  
+  </div>
+</div>
+  
+</body>
+</html>
+
+
